3f06217a5bc3bfaf21e8bf38b366baf6fcf8460c
Mounir1755 <mounirbalikane@outlook.com>
2025-09-19 21:48:07 +0200

    Voeg Controller logica toe om Model data naar view te sturen

--- app/Http/Controllers/OrganisatorController.php
+++ app/Http/Controllers/OrganisatorController.php
@@ -7,12 +7,26 @@ use Illuminate\Http\Request;
 
 class OrganisatorController extends Controller
 {
+    //defiëneren Property
+    private $OrganisatorModel;
+
+    public function __construct()
+    {
+        //Object maken en toevoegen aan Property zodat model kan aangeroepen worden
+        $this->OrganisatorModel = new OrganisatorModel();
+    }
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
+        //opslaan results in $verkopers
+        $verkopers = $this->OrganisatorModel->sp_GetAllverkopers();
 
+        //weergeven view en data meegeven
+        return view('verkopers.index', [
+            'verkopers' => $verkopers
+        ]);
     }
 
     /**
