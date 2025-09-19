61f5e1691a02b08d366bada4164aba3c97d21c41
Mounir1755 <mounirbalikane@outlook.com>
2025-09-19 21:30:53 +0200

    Functie GetAllVerkopers toegevoegd aan Model

--- app/Models/OrganisatorModel.php
+++ app/Models/OrganisatorModel.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+// zorgt voor toegang to DB
+use Illuminate\Support\Facades\DB;
 
 class OrganisatorModel extends Model
 {
-    //
+    public function sp_GetAllVerkopers() 
+    {
+        //uitvoeren Stored Procedure en resultaten terug geven
+        return DB::select('CALL Sp_GetAllVerkopers');
+    }
 }
