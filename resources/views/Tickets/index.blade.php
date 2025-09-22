891a0a5523f009afb908e9b046b66bb30dc6f697
ddylvianen <87158436+ddylvianen@users.noreply.github.com>
2025-09-22 10:05:34 +0200

    Refactor event display: update card layout and styling for improved visual consistency and readability.

--- resources/views/Tickets/index.blade.php
+++ resources/views/Tickets/index.blade.php
@@ -2,11 +2,10 @@
     <h1 class="text-start align-self-start mb-5 text-2xl font-bold">choose your event</h1>
     <div class="row gap-5" style="width: 100%;">
         @foreach ($evenementen as $event)
-            <a href='{{ route("tickets.show", $event) }}' class="card" style="width: 18rem;">
-                <img src="https://placehold.co/300" class="card-img-top" alt="...">
+            <a href='{{ route("tickets.show", $event) }}' class="card" style="width: 24rem; margin-bottom: 2rem; padding: 1.5rem;">
                 <div class="card-body">
-                    <h5 class="card-title">{{ $event->Naam }}</h5>
-                    <p class="card-text">{{ $event->Locatie }}-{{ $event->Datum }}</p>
+                    <h5 class="card-title" style="font-size: 1.75rem;">{{ $event->Naam }}</h5>
+                    <p class="card-text" style="font-size: 1.2rem;">{{ $event->Locatie }} - {{ $event->Datum }}</p>
                 </div>
             </a>
         @endforeach
