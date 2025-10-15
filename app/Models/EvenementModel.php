a94b6094cb3e692453768cbbd25ac39fcdea2797
Dylan Chelvia <dylchelvia@gmail.com>
2025-10-15 22:47:59 +0200

    Rename Evenementmodel.php to EvenementModel.php (fix case)

--- app/Models/EvenementModel.php
+++ app/Models/EvenementModel.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class EvenementModel extends Model
+class Evenementmodel extends Model
 {
-    //
     protected $table = 'evenements';
-    protected $primaryKey = 'id';
-    public $timestamps = true;
+    protected $fillable = [
+        'Naam','Datum','Locatie','AantalTicketsPerTijdslot',
+        'BeschikbareStands','IsActief','Opmerking',
+    ];
+    public $timestamps = false;
+
+    public function stands() {
+        return $this->hasMany(Standmodel::class, 'EvenementId');
+    }
 }
