43d819a4b169a0093011a9731f5650cd6a1b6b82
ddylvianen <87158436+ddylvianen@users.noreply.github.com>
2025-10-15 21:58:25 +0200

    Refactor EvenementModel: remove unused properties and methods to streamline the model

--- app/Models/Evenementmodel.php
+++ app/Models/Evenementmodel.php
@@ -3,46 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\DB;
 
 class EvenementModel extends Model
 {
+    //
     protected $table = 'evenements';
     protected $primaryKey = 'id';
     public $timestamps = true;
-
-    protected $fillable = [
-        'Naam',
-        'Locatie',
-        'Datum',
-        'IsActief',
-        'Opmerking'
-    ];
-
-    const CREATED_AT = 'DatumAangemaakt';
-    const UPDATED_AT = 'DatumGewijzigd';
-
-    // Relationship: Evenement has many stands
-    public function stands()
-    {
-        return $this->hasMany(StandModel::class, 'EvenementId', 'id');
-    }
-
-    // Relationship: Evenement has many prijzen
-    public function prijzen()
-    {
-        return $this->hasMany(PrijsModel::class, 'EvenementId', 'id');
-    }
-
-    // Static method to get all events using stored procedure
-    public static function getAllEvents()
-    {
-        try {
-            return DB::select('CALL SP_GetAllEvents()');
-        } catch (\Exception $e) {
-            \Illuminate\Support\Facades\Log::error('Error getting all events: ' . $e->getMessage());
-            throw $e;
-        }
-    }
 }
-
